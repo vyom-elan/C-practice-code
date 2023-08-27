2e8378d98f3146610c7179acd0deb56cc144b823
Vyom <97853828+vyom-elan@users.noreply.github.com>
2023-08-28 00:05:15 +0530

    Create leaf_nodes.c

---
+++ leaf_nodes.c
@@ -0,0 +1,37 @@
+//Program that counts the leaf nodes in a given binary tree
+
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+int countLeafNodes(struct Node* root) {
+    if (root == NULL) {
+        return 0;
+    }
+    if (root->left == NULL && root->right == NULL) {
+        return 1;
+    }
+    return countLeafNodes(root->left) + countLeafNodes(root->right);
+}
+int main() {
+    struct Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+    root->right->left = newNode(6);
+    root->right->right = newNode(7);
+    int leafCount = countLeafNodes(root);
+    printf("Number of leaf nodes: %d\n", leafCount);
+    return 0;
+}
