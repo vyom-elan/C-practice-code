730d22475def5705a30d5e5bf14c6e633569819d
Vyom <97853828+vyom-elan@users.noreply.github.com>
2023-09-01 22:13:55 +0530

    Create copyfile.c

---
+++ copyfile.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdio.h>
+
+void main()
+{
+    FILE *fp1, *fp2;
+    char ch;
+    int pos;
+
+    if ((fp1 = fopen("File_1.txt", "r")) == NULL)
+    {
+        printf("\nFile cannot be opened.");
+        return;
+    }
+    else
+    {
+        printf("\nFile opened for copy...\n ");
+    }
+    fp2 = fopen("File_2.txt", "w");
+    fseek(fp1, 0L, SEEK_END);   // File pointer at end of file
+    pos = ftell(fp1);
+    fseek(fp1, 0L, SEEK_SET);   // File pointer set at start
+    while (pos--)
+    {
+        ch = fgetc(fp1);    // Copying file character by character
+        fputc(ch, fp2);
+    }
+    fcloseall();
+}
